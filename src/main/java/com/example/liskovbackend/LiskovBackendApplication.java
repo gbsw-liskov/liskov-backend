a99c0955b333c42ddf7e01561f3cab3c14b5ea0c
SeungYeop Ryu <n006h3417h@gmail.com>
2025-10-15 20:15:02 +0900

    chore: init

---
+++ src/main/java/com/example/liskovbackend/LiskovBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.liskovbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiskovBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiskovBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liskovbackend/LiskovBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liskovbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiskovBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
