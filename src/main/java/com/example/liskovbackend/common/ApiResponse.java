91e069abf915e1adffaf9f1ae2cbeed4ff5dcef9
SeungYeop Ryu <n006h3417h@gmail.com>
2025-10-17 12:27:56 +0900

    feat: global response class

---
+++ src/main/java/com/example/liskovbackend/common/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.example.liskovbackend.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter @Setter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ResponseEntity<ApiResponse<T>> ok(T data) {
+        return ResponseEntity.ok(new ApiResponse<>(true, "success", data));
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> error(String message, HttpStatus status) {
+        return ResponseEntity.status(status)
+            .body(new ApiResponse<>(false, message, null));
+    }
+}
