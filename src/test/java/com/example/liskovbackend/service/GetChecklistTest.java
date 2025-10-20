88120c15d0df4cabeeb56fd4b6d1477f07cebb80
Knhye <knahye311@gmail.com>
2025-10-21 01:53:08 +0900

    test: GetChecklistServiceTest 수정＂

--- src/test/java/com/example/liskovbackend/service/GetChecklistTest.java
+++ src/test/java/com/example/liskovbackend/service/GetChecklistTest.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ChecklistServiceTest {
+class GetChecklistServiceTest {
 
     @Mock
     private ChecklistRepository checklistRepository;
