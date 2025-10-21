d5a5587581dfe354e967bbfcac3a3764f7b6b8f6
Knhye <knahye311@gmail.com>
2025-10-21 23:28:39 +0900

    test: 체크리스트 수정 기능에 대한 서비스 테스트 추가

--- src/main/java/com/example/liskovbackend/dto/ChecklistUpdateRequest.java
+++ src/main/java/com/example/liskovbackend/dto/ChecklistUpdateRequest.java
@@ -1,9 +1,11 @@
 package com.example.liskovbackend.dto;
 
 import com.example.liskovbackend.enums.Severity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ChecklistUpdateRequest {
     private Long itemId;
     private String memo;
