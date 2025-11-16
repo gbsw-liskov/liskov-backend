2e22231aa1795b319b7462c1cfc421b5ac06c8fd
SeungYeop Ryu <n006h3417h@gmail.com>
2025-11-16 11:34:08 +0900

    fix: 엔드포인트 경로 수정

--- src/main/java/com/example/liskovbackend/controller/AnalysisController.java
+++ src/main/java/com/example/liskovbackend/controller/AnalysisController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/analyze")
+@RequestMapping("/api/analysis")
 public class AnalysisController {
 
     private final AnalysisService analysisService;
