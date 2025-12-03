4c8e900946ec27069f321dc7f24111ee08383623
SeungYeop Ryu <n006h3417h@gmail.com>
2025-12-04 03:04:22 +0900

    feat: CORS 비활성화

--- src/main/java/com/example/liskovbackend/config/SecurityConfig.java
+++ src/main/java/com/example/liskovbackend/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
