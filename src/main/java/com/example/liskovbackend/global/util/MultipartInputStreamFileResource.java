8ce8d9f921bc07c8345b2e688d13b00f08bd6e69
SeungYeop Ryu <n006h3417h@gmail.com>
2025-12-24 18:56:51 +0900

    fix: 요청 바디 수정

---
+++ src/main/java/com/example/liskovbackend/global/util/MultipartInputStreamFileResource.java
@@ -0,0 +1,27 @@
+package com.example.liskovbackend.global.util;
+
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public class MultipartInputStreamFileResource extends InputStreamResource {
+
+    private final String filename;
+
+    public MultipartInputStreamFileResource(MultipartFile file) throws IOException {
+        super(file.getInputStream());
+        this.filename = file.getOriginalFilename();
+    }
+
+    @Override
+    public String getFilename() {
+        return this.filename;
+    }
+
+    @Override
+    public long contentLength() {
+        return -1;
+    }
+}
